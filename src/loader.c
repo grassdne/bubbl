083a9a5235ff1ebf2ae5375b0f800ca8a95e3ead
RZia <36330543+grassdne@users.noreply.github.com>
2022-08-20 12:25:18 -0400

    improve io error handling

--- src/loader.c
+++ src/loader.c
@@ -5,16 +5,22 @@
 const char* mallocShaderSource(const char* fpath) {
 	FILE* f;
 	if ((f = fopen(fpath, "r")) == NULL) {
-		fprintf(stderr, "File (%s): %s", fpath, ERROR());
+		fprintf(stderr, "Unable to open file (%s): %s", fpath, ERROR());
 		exit(1);
 	}
-	fseek(f, 0, SEEK_END);
+	if (fseek(f, 0, SEEK_END)) {
+        fprintf(stderr, "Unable to seek file (%s): %s", fpath, ERROR());
+        exit(1);
+    }
 	size_t size = ftell(f);
-	fseek(f, 0, SEEK_SET);
+    rewind(f);
 
 	char* s = malloc(size + 1);
+    if (s == NULL) {
+        exit(1);
+    }
 	if ((fread(s, sizeof(char), size, f)) == 0) {
-		fprintf(stderr, "File (%s): %s", fpath, ERROR());
+		fprintf(stderr, "Unable to read file (%s): %s", fpath, ERROR());
 		exit(1);
 	}
     fclose(f);
