88d810b96c646d4fe4bb338180d6a6c4fdaf0fcc
RZia <36330543+grassdne@users.noreply.github.com>
2022-08-27 11:06:20 -0400

    fix frame glitch & windows crash
    When the bubble pops by getting too big, there would be a shader program
    mixup.

--- src/bubbleshader.c
+++ src/bubbleshader.c
@@ -314,16 +314,6 @@ void bubbleInit(BubbleShader *sh) {
 }
 
 void bubbleOnDraw(BubbleShader *sh, double dt) {
-    // Bind
-	glUseProgram(sh->program);
-    glBindVertexArray(sh->vertex_array);
-    glBindBuffer(GL_ARRAY_BUFFER, sh->bubble_vbo);
-
-    // Update state
-    FOR_ACTIVE_BUBBLES(i) {
-        update_position(sh, dt, i);
-    }
-
     if (GROWING().alive) {
         GROWING().rad += GROWING_RDELTA * dt;
         if (GROWING().rad >= MAX_RADIUS) {
@@ -331,7 +321,18 @@ void bubbleOnDraw(BubbleShader *sh, double dt) {
             GROWING().alive = false;
         }
     }
+    // Update state
+    FOR_ACTIVE_BUBBLES(i) {
+        update_position(sh, dt, i);
+    }
+
     check_collisions(sh);
+
+    // Bind
+	glUseProgram(sh->program);
+    glBindVertexArray(sh->vertex_array);
+    glBindBuffer(GL_ARRAY_BUFFER, sh->bubble_vbo);
+
     
     // Update buffer
     glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(sh->bubbles), sh->bubbles);
