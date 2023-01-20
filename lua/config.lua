6a819076b700d24aa13fa44253d43cf6c0fcd278
grassdne <ziarustum@gmail.com>
2023-01-20 18:36:20 -0500

    put config in config.lua

--- lua/config.lua
+++ lua/config.lua
@@ -1,2 +1,25 @@
---require "elasticbubbles"
-require "rainbow"
+CONFIG = {
+    ELASTIC_BUBBLE = {
+        STARTING_BUBBLE_COUNT = 10;
+        BUBBLE_SPEED_BASE = 200;
+        BUBBLE_SPEED_VARY = 225;
+        BUBBLE_RAD_BASE = 30;
+        BUBBLE_RAD_VARY = 25;
+        MAX_GROWTH = 200;
+        MIN_GROWTH_RATE = 50;
+        MAX_GROWTH_RATE = 225;
+        TRANS_IMMUNE_PERIOD = 1;
+        TRANS_TIME = 1;
+        POP_EXPAND_MULT = 2.0;
+        POP_LAYER_WIDTH = 10.0;
+        POP_PARTICLE_LAYOUT = 5;
+        POP_LIFETIME = 1.0;
+        POP_PT_RADIUS = 7.0;
+        POP_PT_RADIUS_DELTA = 4.0;
+        BUBBLE_HUE = 0.9,
+        BUBBLE_LIGHTNESS = 0.5,
+    }
+}
+
+require "elasticbubbles"
+--require "rainbow"
--- lua/elasticbubbles.lua
+++ lua/elasticbubbles.lua
@@ -1,25 +1,6 @@
 title = "Elastic Bubbles"
 
-TWEAK = {
-    STARTING_BUBBLE_COUNT = 10;
-    BUBBLE_SPEED_BASE = 200;
-    BUBBLE_SPEED_VARY = 225;
-    BUBBLE_RAD_BASE = 30;
-    BUBBLE_RAD_VARY = 25;
-    MAX_GROWTH = 200;
-    MIN_GROWTH_RATE = 50;
-    MAX_GROWTH_RATE = 225;
-    TRANS_IMMUNE_PERIOD = 1;
-    TRANS_TIME = 1;
-    POP_EXPAND_MULT = 2.0;
-    POP_LAYER_WIDTH = 10.0;
-    POP_PARTICLE_LAYOUT = 5;
-    POP_LIFETIME = 1.0;
-    POP_PT_RADIUS = 7.0;
-    POP_PT_RADIUS_DELTA = 4.0;
-    BUBBLE_HUE = 0.9,
-    BUBBLE_LIGHTNESS = 0.5,
-}
+local TWEAK = CONFIG.ELASTIC_BUBBLE
 
 local ffi = require "ffi"
 local add_bubble = function (bubble)
