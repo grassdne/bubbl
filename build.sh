7c493920b15690e71e08f8417199548b12ec0237
grassdne <ziarustum@gmail.com>
2023-02-18 11:28:52 -0500

    [build.sh] clang

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ set -e # Quit on error
 
 DEPS="luajit sdl2 glew"
 
-CFLAGS="-Wall -Wextra -std=c11 --pedantic $(pkg-config --cflags $DEPS)"
+CFLAGS="-Wall -Wextra -std=c11 --pedantic -Ideps $(pkg-config --cflags $DEPS)"
 CLIBS="$(pkg-config --libs $DEPS) -lm"
 
 if [ `uname` = Darwin ]; then
@@ -21,4 +21,4 @@ else
 fi
 
 set -x # Echo commmand
-${CC:=cc} -o bubbl -Ideps src/all.c $CFLAGS $CLIBS 
+${CC:=cc} -o bubbl src/all.c $CFLAGS $CLIBS 
