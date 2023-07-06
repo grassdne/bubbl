410ba411afcb909dfe3a9ed3b352b8d1b57893cb
grassdne <ziarustum@gmail.com>
2023-07-06 19:10:13 -0400

    move api functions to separate file

--- lua/eventloop.lua
+++ lua/eventloop.lua
@@ -26,7 +26,7 @@ local OnKey = function(key, is_down)
         -- Unlock global table
         setmetatable(_G, nil)
         for k,v in pairs(package.loaded) do package.loaded[k] = nil end
-        require 'config'
+        require 'loader'
         OptionalCallback("OnStart", true)
     end
     OptionalCallback("OnKey", key, is_down)
