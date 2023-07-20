7aa1bd118aa9bc8d176da68dbbd877fc1c80191b
grassdne <ziarustum@gmail.com>
2023-07-20 11:33:34 -0400

    [draw] naming inconsistency

--- lua/draw.lua
+++ lua/draw.lua
@@ -4,36 +4,36 @@ local POINT_SIZE=3
 
 local draw = {}
 
-draw.point = function(x, y, color)
+draw.Point = function(x, y, color)
     RenderPop(Vector2(x, y), color or Color.hex "000000", POINT_SIZE)
 end
-draw.horiz_line = function(x1, y1, w, c)
-    for x=x1, x1+w, POINT_SIZE do draw.point(x, y1, c) end
+draw.HorizontalLine = function(x1, y1, w, c)
+    for x=x1, x1+w, POINT_SIZE do draw.Point(x, y1, c) end
 end
-draw.vert_line = function(x1, y1, w, c)
-    for y=y1, y1+w, POINT_SIZE do draw.point(x1, y, c) end
+draw.VerticalLine = function(x1, y1, w, c)
+    for y=y1, y1+w, POINT_SIZE do draw.Point(x1, y, c) end
 end
 
-draw.line = function(x1, y1, x2, y2, color)
+draw.Line = function(x1, y1, x2, y2, color)
     local vector = Vector2(x2-x1, y2-y1)
     local angle = vector:normalize()
     local count = vector:length() / POINT_SIZE
     for i = 0, count-1 do
         local pos = Vector2(x1, y1) + angle * (i * POINT_SIZE)
-        draw.point(pos.x, pos.y, color)
+        draw.Point(pos.x, pos.y, color)
     end
     local diff_x = (x2 - x1) / POINT_SIZE
 end
 
-draw.rect_outline = function(x1, y1, w, h, c)
+draw.RectOutline = function(x1, y1, w, h, c)
     -- Bottom
-    draw.horiz_line(x1, y1, w, c)
+    draw.HorizontalLine(x1, y1, w, c)
     -- Top
-    draw.horiz_line(x1, y1+h, w, c)
+    draw.HorizontalLine(x1, y1+h, w, c)
     -- Left
-    draw.vert_line(x1, y1, h, c)
+    draw.VerticalLine(x1, y1, h, c)
     -- Right
-    draw.vert_line(x1+w, y1, h, c)
+    draw.VerticalLine(x1+w, y1, h, c)
 end
 
 return draw
--- modules/svgeditor.lua
+++ modules/svgeditor.lua
@@ -79,19 +79,19 @@ Draw = function(dt)
     end
 
     local base = get_draw_box_base_position()
-    draw.rect_outline(base.x, base.y, TextRenderer.GLYPH_WIDTH*scale, TextRenderer.GLYPH_HEIGHT*scale, WEBCOLORS.BLACK)
+    draw.RectOutline(base.x, base.y, TextRenderer.GLYPH_WIDTH*scale, TextRenderer.GLYPH_HEIGHT*scale, WEBCOLORS.BLACK)
 
     if selection_start then
         local x1, y1, x2, y2 = GetSelection()
         local botleft = AbsolutePosition(Vector2(x1, y1))
         local topright = AbsolutePosition(Vector2(x2, y2))
-        draw.rect_outline(botleft.x, botleft.y, topright.x - botleft.x, topright.y - botleft.y, SVGEDITOR.COLOR)
+        draw.RectOutline(botleft.x, botleft.y, topright.x - botleft.x, topright.y - botleft.y, SVGEDITOR.COLOR)
     end
 
      if rotate then
          local axis = AbsolutePosition(rotate.axis_position)
          local mouse = MousePosition()
-         draw.line(axis.x, axis.y, mouse.x, mouse.y, SVGEDITOR.COLOR)
+         draw.Line(axis.x, axis.y, mouse.x, mouse.y, SVGEDITOR.COLOR)
      end
 
     -- Testing text
