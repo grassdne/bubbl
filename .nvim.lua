f36339fd9e3d6300aeb17fa3bb3b94ba39cbf685
grassdne <ziarustum@gmail.com>
2023-08-03 13:48:08 -0400

    add .nvim.lua

---
+++ .nvim.lua
@@ -0,0 +1,15 @@
+local reload = function ()
+    local cmd = "curl -X POST http://localhost:3636/action/reload"
+    print(cmd)
+    vim.fn.system(cmd)
+end
+
+vim.api.nvim_create_augroup("bubbl", {})
+vim.api.nvim_create_autocmd("BufWritePost", {
+    pattern = "*.lua",
+    group = "bubbl",
+    callback = function() vim.defer_fn(reload, 0) end,
+})
+
+vim.api.nvim_create_autocmd("FileType", { pattern = "lua", command = "set makeprg=./bubbl" })
+vim.api.nvim_create_autocmd("FileType", { pattern = "c", command = "set makeprg=./build.lua" })
