c109276124df42dec065c66260f665e4a202370f
Rustum Zia <ziarustum@gmail.com>
2023-11-25 12:50:02 -0500

    [text.lua] LiberationMono-cluster

--- lua/text.lua
+++ lua/text.lua
@@ -137,6 +137,7 @@ end
 TextRenderer.SetFont("Lora-VariableFont")
 -- TextRenderer.SetFont("LiberationSans-Regular")
 -- TextRenderer.SetFont("LiberationMono-Regular")
+-- TextRenderer.SetFont("LiberationMono-cluster")
 -- TextRenderer.SetFont("funky")
 
 return TextRenderer
